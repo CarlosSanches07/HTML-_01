a61ff72314a321d267e101e032f79e2291c72992
CarlosSanches07 <carlos-caps@holtmail.com>
2017-10-30 16:15:54 -0200

    final commit ?

--- controllers/controller.content.js
+++ controllers/controller.content.js
@@ -1,11 +1,15 @@
 export default class controllerContent {
 	
-	static updateById(req,database){
-		let content =[req.body.title, req.body.description,req.body.id];
-		let string = 'update content set title = $1, description = $2 where id = $3';
-		database.query(string, content, (err,data)=>{
-			if(err)
-				console.log(err);
-		} )	
+	static updateById(req, res, database){
+		if(req.body.title == "" || req.body.description == "")
+			res.render('admin',{'user' : req.session.login, 'contentErr' : 'err'})	
+		else{	
+			let content =[req.body.title, req.body.description,req.session.login];
+			let string = 'insert into content(title, description, humanId) values($1,$2,$3)';
+			database.query(string, content, (err,data)=>{
+				if(err)
+					console.log(err);
+			})	
+		}
 	}
 }
--- controllers/controller.human.js
+++ controllers/controller.human.js
@@ -2,8 +2,8 @@ export default class controllerHuman{
 	static verifyLogin(req,res,database){
 		let query = {
 			name : 'verify-logn',
-			text : 'select id from human where $1 = human.name and md5($2) = human.password',
-			values : [req.body.login, req.body.password]
+			text : 'select id from human where $1 = human.email and md5($2) = human.password',
+			values : [req.body.email, req.body.password]
 		}
 		database.query(query)
 			.then((data) => {
@@ -18,38 +18,33 @@ export default class controllerHuman{
 	}
 
 	static verifyEmail(req, res, database){
-		let query = {
-			name : 'verify-email',
-			text : 'select id from human where $1 = human.email',
-			values : [req.body.email]
-		}
-		database.query(query)
-			.then((data)=>{
-				if(!data.rows[0]){
-					let query2 = {
-						name : 'insert-human',
-						text : 'insert into human(name, email, password) values($1, $2, md5($3))',
-						values : [req.body.name,req.body.email,req.body.password]
+		if(req.body.name == "" || req.body.email == "" || req.body.adress == "" || req.body.password == "")
+			res.render('admin',{'fielderr' : 'insertHuman', 'user' : req.session.login})
+		else{
+			let query = {
+				name : 'verify-email',
+				text : 'select id from human where $1 = human.email',
+				values : [req.body.email]
+			}
+			database.query(query)
+				.then((data)=>{
+					if(!data.rows[0]){
+						let query2 = {
+							name : 'insert-human',
+							text : 'insert into human(name, email, adress, password) values($1, $2, $3, md5($4))',
+							values : [req.body.name,req.body.email, req.body.adress,req.body.password]
+						}
+						database.query(query2)
+						.then(()=>{
+							res.redirect('/admin');
+						})
+						.catch((err)=>console.log(err))
+					}else{
+						res.render('admin',{erro:'erro'});
 					}
-					database.query(query2)
-					.then(()=>{
-						res.redirect('/admin');
-					})
-					.catch((err)=>console.log(err))
-				}else{
-					res.render('admin',{erro:'erro'});
-				}
-			})
-			.catch(err => console.log(err))
+				})
+				.catch(err => console.log(err))
+		}
 	}
 
-	/*static insertHuman(req,res,database,verifyEmail){
-		
-		if(verifyEmail){
-
-		}
-		else{
-			
-		}
-	}*/
 }
